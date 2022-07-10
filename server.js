85c5af592145aea164fa8ff809abbe44b732f5c3
DawnWogerman <99206850+DawnWogerman@users.noreply.github.com>
2022-07-10 09:10:43 -0600

    initial commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const express = require('express');
+const PORT = process.env.PORT || 3001;
+const app = express();
+const path = require('path');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3001, () =>{
+    console.log(`API server now on port 3001`);
+});
