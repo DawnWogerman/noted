850f041955c895c29f8e42325a271887505c7ddb
DawnWogerman <99206850+DawnWogerman@users.noreply.github.com>
2022-07-17 09:54:57 -0600

    deploying to heroku

--- server.js
+++ server.js
@@ -26,6 +26,6 @@ app.use('/', htmlRoutes);
 
 
 
-app.listen(3001, () =>{
-    console.log(`API server now on port 3001`);
+app.listen(PORT, () =>{
+    console.log(`API server now on port ${PORT}`);
 });
